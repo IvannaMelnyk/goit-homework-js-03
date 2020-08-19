6badb82ef7c7e43de52cf49c7696cd0cc8d229bd
IvannaMelnyk <givmiller@gmail.com>
2020-08-19 23:09:33 +0300

    m5
    функция getAllPropValues(arr, prop), которая получает массив объектов и имя свойства.
    
    Функция возвращает массив значений определенного свойства prop из каждого объекта в массиве.
    
    Используй метод push для добавления значения в массив и оператор in для проверки наличия свойства в объекте.

--- js/module5.js
+++ js/module5.js
@@ -1,17 +1,25 @@
-function checkForSpam(str) {
+function getAllPropValues(array, prop) {
     'use strict';
-    // Write code under this line
-    const string = str.toLowerCase();
-    const checkedString =
-        string.includes('spam') || string.includes('sale') ? true : false;
+    // Write code under this line 
 
-    return checkedString;
 }
 
-console.log(checkForSpam('Latest technology news')); // false
+// Объекты и ожидаемый результат
+const products = [
+    { name: 'Радар', price: 1300, quantity: 4 },
+    { name: 'Радар', price: 1280, quantity: 2 },
+    { name: 'Радар', price: 1320, quantity: 1 },
+    { name: 'Сканер', price: 2700, quantity: 1 },
+    { name: 'Сканер', price: 2500, quantity: 3 },
+    { name: 'Дроид', price: 400, quantity: 7 },
+    { name: 'Захват', price: 1200, quantity: 2 },
+];
 
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
+//console.log(getAllPropValues(products, 'name'));
+// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
 
-console.log(checkForSpam('Get best sale offers now!')); // true
+//console.log(getAllPropValues(products, 'quantity'));
+// [4, 2, 1, 1, 3, 7, 2]
 
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+//console.log(getAllPropValues(products, 'category'));
+//  []
